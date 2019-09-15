bfad9f61c8f74ef253d6e01d53111daebe5bfb22
Mikel-Jon West <dwmyke@gmail.com>
2019-09-14 23:29:47 -0700

    Adjusted gatsbyconfig

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,6 @@ module.exports = {
   siteMetadata: {
     title: `Fawn Brooks`,
     description: `Environmental Research and Data Analysis`,
-    author: `Fawn Brooks,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -11,6 +10,26 @@ module.exports = {
       options: {
         name: `images`,
         path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: `gatsby-source-wordpress`,
+      options: {
+        /*
+         * The base URL of the WordPress site without the trailingslash and the protocol. This is required.
+         * Example : 'dev-gatbsyjswp.pantheonsite.io' or 'www.example-site.com'
+         */
+        baseUrl: `blog.fawnbrooks.com`,
+        // The protocol. This can be http or https.
+        protocol: `https`,
+        // Indicates whether the site is hosted on wordpress.com.
+        // If false, then the assumption is made that the site is self hosted.
+        // If true, then the plugin will source its content on wordpress.com using the JSON REST API V2.
+        // If your site is hosted on wordpress.org, then set this to false.
+        hostingWPCOM: false,
+        // If useACF is true, then the source plugin will try to import the WordPress ACF Plugin contents.
+        // This feature is untested for sites hosted on WordPress.com
+        useACF: true,
       },
     },
     `gatsby-transformer-sharp`,
